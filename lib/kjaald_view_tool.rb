4e0ab72ce3bf1e7b8eb787cd03b9571d194f6ddc
Robin Neu <robin.neu@devinci.fr>
2019-06-10 20:07:15 +0200

    Dev tool initial commit

---
+++ lib/kjaald_view_tool.rb
@@ -0,0 +1,6 @@
+require "kjaald_view_tool/version"
+require "kjaald_view_tool/renderer"
+module KjaaldViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/kjaald_view_tool/version.rb
@@ -0,0 +1,3 @@
+module KjaaldViewTool
+  VERSION = "0.1.0"
+end
